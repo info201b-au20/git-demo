0d5299c66663e28b23bfc6a740bc191f48d2c92d
mkfreeman <michael.k.freeman@gmail.com>
2020-11-02 15:11:25 -0500

    Draw sweet ggplot stuff

--- analysis.R
+++ analysis.R
@@ -1,1 +1,9 @@
 # Do cool stuff
+library(tidyverse)
+tibble(t = (1:2300)*2.4432, 
+       x = sin(t), 
+       y = cos(t)) %>% 
+  ggplot(aes(x*t, y*t)) +
+  geom_point(size=58, alpha=.1, shape=13)+ 
+  coord_equal() +
+  theme_void()
