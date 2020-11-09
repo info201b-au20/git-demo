68c98034e72a572d99b56bbf090ff6446e78576f
mkfreeman <michael.k.freeman@gmail.com>
2020-11-09 13:55:57 -0500

    Make a cool plot

--- analysis.R
+++ analysis.R
@@ -2,12 +2,12 @@
 # sorry u got rejected Mike :/
 # From: https://twitter.com/aschinchon
 library(tidyverse)
-tibble(t = (1:2300)*2.4432, 
-       x = cos(t), 
+my_plot <- tibble(t = (1:1300)*2.4432, 
+       x = sin(t), 
        y = cos(t)) %>% 
   ggplot(aes(x*t, y*t)) +
-  geom_point(size=58, alpha=.1, shape=13)+ 
+  geom_point(size=58, alpha=.1, shape=14)+ 
   coord_equal() +
   theme_void()
 
-
+ggsave("mikes_plot.png", my_plot)
