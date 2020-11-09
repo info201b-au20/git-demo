272cfc508bf8938db8fbfaf92c3ed91bead5e85c
jcostales10 <costaj28@uw.edu>
2020-11-09 10:55:34 -0800

    add jamie's plot

--- analysis.R
+++ analysis.R
@@ -2,11 +2,11 @@
 # sorry u got rejected Mike :/
 # From: https://twitter.com/aschinchon
 library(tidyverse)
-tibble(t = (1:2300)*2.4432, 
+jamies_plot <- tibble(t = (1:2300)*2.4432, 
        x = cos(t), 
-       y = cos(t)) %>% 
+       y = sin(t)) %>% 
   ggplot(aes(x*t, y*t)) +
-  geom_point(size=58, alpha=.1, shape=13)+ 
+  geom_point(size=80, alpha=.3, shape=13)+ 
   coord_equal() +
   theme_void()
 
