e6f733c2022f893c2331d6f29fc0cd8b9799c119
mkfreeman <michael.k.freeman@gmail.com>
2020-11-09 13:51:19 -0500

    Switch cos to sin

--- analysis.R
+++ analysis.R
@@ -3,7 +3,7 @@
 # From: https://twitter.com/aschinchon
 library(tidyverse)
 tibble(t = (1:2300)*2.4432, 
-       x = sin(t), 
+       x = cos(t), 
        y = cos(t)) %>% 
   ggplot(aes(x*t, y*t)) +
   geom_point(size=58, alpha=.1, shape=13)+ 
